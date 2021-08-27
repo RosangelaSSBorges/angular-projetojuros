94f96c7f5149abbd1c17631e3d2b52f812f22f6a
RosangelaSSBorges <rosangelassb@hotmail.com>
2021-08-27 19:34:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ph6hka
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ph6hka)
---
+++ src/app/juros-acumulados/juros-acumulados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros-acumulados',
+  templateUrl: './juros-acumulados.component.html',
+  styleUrls: ['./juros-acumulados.component.css']
+})
+export class JurosAcumuladosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
